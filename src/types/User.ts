460046ac025eafbdfa833e4d62a288fd85f60de1
tom <thomas.munro@btinternet.com>
2020-03-15 17:58:45 +0000

    🚚 migrate user to ts

---
+++ src/types/User.ts
@@ -0,0 +1,41 @@
+class User {
+  id: string;
+  displayName: string;
+  isModerator: boolean;
+  email: string;
+  emailVerified: boolean;
+  isAnonymous: boolean;
+  phoneNumber: string;
+  photoURL: string;
+  description: string;
+  location: any;
+  profileURL: string;
+
+  constructor(
+    id: string,
+    displayName: string,
+    isModerator: boolean,
+    email: string,
+    emailVerified: boolean,
+    isAnonymous: boolean,
+    phoneNumber: string,
+    photoURL: string,
+    description: string,
+    location: any,
+    profileURL: string
+  ) {
+    this.id = id;
+    this.displayName = displayName;
+    this.isModerator = isModerator;
+    this.email = email;
+    this.emailVerified = emailVerified;
+    this.isAnonymous = isAnonymous;
+    this.phoneNumber = phoneNumber;
+    this.photoURL = photoURL;
+    this.description = description;
+    this.location = location;
+    this.profileURL = profileURL;
+  }
+}
+
+export default User;
