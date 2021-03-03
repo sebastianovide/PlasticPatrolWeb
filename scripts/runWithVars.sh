1382ec2496589cc534ae0a6be4ae90d1ae033422
Sebastian Ovide <sebastian.ovide@gmail.com>
2021-03-03 08:27:18 +0000

    build prod version (#233)

--- scripts/runWithVars.sh
+++ scripts/runWithVars.sh
@@ -11,7 +11,8 @@ cp node_modules/first-input-delay/dist/first-input-delay.min.js  public/
 VERSION_MAYOR=$(echo $npm_package_version| cut -d'.' -f 1)
 VERSION_MINOR=$(echo $npm_package_version| cut -d'.' -f 2)
 
-export REACT_APP_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER:-"0"}
+BUILD_NUMBER=${TRAVIS_BUILD_NUMBER:-$GITHUB_RUN_NUMBER} 
+export REACT_APP_BUILD_NUMBER=${BUILD_NUMBER:-"0"}
 export REACT_APP_VERSION="$VERSION_MAYOR.$VERSION_MINOR.$REACT_APP_BUILD_NUMBER"
 export REACT_APP_TITLE=$npm_package_title
 
