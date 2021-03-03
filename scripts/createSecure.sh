ac43ae8a75bcda97b24f5e626fc29db0cfec9029
Sebastian Ovide <sebastian.ovide@gmail.com>
2021-03-03 15:50:55 +0000

    -md sha512 (#239)

--- scripts/createSecure.sh
+++ scripts/createSecure.sh
@@ -10,7 +10,7 @@ travis encrypt-file secrets.tar secrets.tar.enc -p -r Geovation/plastic-patrol
 # this is used for github
 # make sure that $ENC_PASSWORD is in github
 tar cvf secrets.tar build.json *.cer *.p12 *.mobileprovision *.keystore serviceAccountKey-plastic-patrol.json GooglePlayAndroidDeveloper.json
-openssl enc -aes-256-cbc -in secrets.tar -out secrets.tar.encrypted -k $ENC_PASSWORD
+openssl enc -aes-256-cbc -in secrets.tar -out secrets.tar.encrypted -k $ENC_PASSWORD -md sha512
 
 # echo you should run this command
 # echo "cp secrets.tar ../secrets/plastic-patrol.tar"
