286520a737a1461d1f0382c73320f8865174ff36
Joy Shan-Chun Kuo <shanchun.kuo@geovation.uk>
2021-03-04 18:44:41 +0000

    Update ios-testflight.yml: add keychain password

--- scripts/add-key-github.sh
+++ scripts/add-key-github.sh
@@ -7,12 +7,12 @@ set -eo pipefail
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp "./cordova-app/Geovation.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
 
-security create-keychain -p "" build.keychain
+security create-keychain -p github build.keychain
 security import ./cordova-app/dist.cer -t agg -k ~/Library/Keychains/build.keychain -P "" -A
 security import ./cordova-app/apple.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+security unlock-keychain -p github ~/Library/Keychains/build.keychain
 
-security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k "" ~/Library/Keychains/build.keychain
+security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k github ~/Library/Keychains/build.keychain
