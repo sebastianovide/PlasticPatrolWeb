f9644309320413580dc19bc5da05b77e8598b3a7
Sebastian Ovide <sebastian.ovide@gmail.com>
2021-03-04 07:24:50 +0000

    try to add certificates (#240)

---
+++ scripts/add-key-github.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eo pipefail
+
+# see https://engineering.talkdesk.com/test-and-deploy-an-ios-app-with-github-actions-44de9a7dcef6
+
+# Put the provisioning profile in place
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+cp "./cordova-app/Geovation.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
+
+security create-keychain -p "" build.keychain
+security import ./cordova-app/dist.cer -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+
+security list-keychains -s ~/Library/Keychains/build.keychain
+security default-keychain -s ~/Library/Keychains/build.keychain
+security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+
+security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
