def2efb0452c842f094cced64108424abb2f0b6b
Joy Shan-Chun Kuo <shanchun.kuo@geovation.uk>
2021-03-09 22:22:08 +0000

    Update secrets.tar.encrypted and signed apk for Android beta (#263)

--- scripts/createSecure.sh
+++ scripts/createSecure.sh
@@ -4,12 +4,12 @@ pushd cordova-app
 
 # this is used for travis and will eventually be removed
 
-tar cvf secrets.tar build.json *.cer *.p12 *.mobileprovision *.keystore serviceAccountKey-plastic-patrol.json GooglePlayAndroidDeveloper.json
-travis encrypt-file secrets.tar secrets.tar.enc -p -r Geovation/plastic-patrol
+# tar cvf secrets.tar build.json *.cer *.p12 *.mobileprovision *.jks GoogleService-Info.plist google-services.json GooglePlayAndroidDeveloper.json
+# travis encrypt-file secrets.tar secrets.tar.enc -p -r Geovation/plastic-patrol
 
 # this is used for github
 # make sure that $ENC_PASSWORD is in github
-tar cvf secrets.tar build.json *.cer *.p12 *.mobileprovision *.keystore serviceAccountKey-plastic-patrol.json GooglePlayAndroidDeveloper.json
+tar cvf secrets.tar build.json *.cer *.p12 *.mobileprovision *.jks GoogleService-Info.plist google-services.json GooglePlayAndroidDeveloper.json
 openssl enc -aes-256-cbc -in secrets.tar -out secrets.tar.encrypted -k $ENC_PASSWORD -md sha512
 
 # echo you should run this command
