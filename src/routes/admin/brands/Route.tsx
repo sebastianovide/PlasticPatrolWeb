7e6a49c963e26141d0465e19e4c41dc8e0821ef4
tom <thomas.munro@btinternet.com>
2021-03-13 17:32:54 +0000

    🐛 link up back button

--- src/routes/admin/brands/Route.tsx
+++ src/routes/admin/brands/Route.tsx
@@ -1,10 +1,15 @@
 import PageWrapper from "components/PageWrapper";
 import BrandsManagementPage from "pages/admin/brands/Brands";
 import React from "react";
+import { useHistory } from "react-router";
 
 export default function BrandsRoute() {
+  const history = useHistory();
   return (
-    <PageWrapper label="Brands" navigationHandler={{ handleBack: () => {} }}>
+    <PageWrapper
+      label="Brands"
+      navigationHandler={{ handleBack: () => history.push("/") }}
+    >
       <BrandsManagementPage />
     </PageWrapper>
   );
--- src/routes/admin/categories/Route.tsx
+++ src/routes/admin/categories/Route.tsx
@@ -1,12 +1,14 @@
 import PageWrapper from "components/PageWrapper";
 import CategoriesManagementPage from "pages/admin/categories/Categories";
 import React from "react";
+import { useHistory } from "react-router";
 
 export default function CategoriesRoute() {
+  const history = useHistory();
   return (
     <PageWrapper
       label="Categories"
-      navigationHandler={{ handleBack: () => {} }}
+      navigationHandler={{ handleBack: () => history.push("/") }}
     >
       <CategoriesManagementPage />
     </PageWrapper>
