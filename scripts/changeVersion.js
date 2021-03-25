661104cc4d725deb866382316fe14cb332c4ffa8
Joy Shan-Chun Kuo <shanchun.kuo@geovation.uk>
2021-03-25 17:21:19 +0000

    Update script: build number and version code for Android and iOS (#322)

--- scripts/changeVersion.js
+++ scripts/changeVersion.js
@@ -9,16 +9,22 @@ const version = process.env.REACT_APP_VERSION || "1.1.1";
 
 // Android
 const versionSplit = version.split(".");
-const newAndroidVersionName = versionSplit[0] * 10000000 + versionSplit[1] * 10000 + versionSplit[2];
-console.log("android-versionName:", newAndroidVersionName);
+const newAndroidVersionCode = versionSplit[0] * 10000000 + versionSplit[1] * 10000 + versionSplit[2];
+console.log("android-versionCode:", newAndroidVersionCode);
+console.log("android-versionName:", version);
 const gradleContent = readFileSync(gradleFilePath, 'utf8');
-const regex = /versionName \".*\"/;
-const newGradleContent = gradleContent.replace(regex, `versionName "${newAndroidVersionName}"`);
-writeFileSync(gradleFilePath, newGradleContent)
+const regexVersionCode = /versionCode [0-9]+/;
+const regexVersionName = /versionName \".*\"/;
+const newGradleContent = gradleContent.replace(regexVersionCode, `versionCode ${newAndroidVersionCode}`);
+const latestGradleContent = newGradleContent.replace(regexVersionName, `versionName "${version}"`);
+writeFileSync(gradleFilePath, latestGradleContent);
 
 // IOS
+const newIosCFBundleVersion = version;
 const newIosCFBundleShortVersionString = version;
+console.log("ios-CFBundleVersion:", newIosCFBundleVersion);
 console.log("ios-CFBundleShortVersionString:", newIosCFBundleShortVersionString);
 const obj = plist.parse(readFileSync(infoFilePath, 'utf8'));
+obj.CFBundleVersion = newIosCFBundleVersion;
 obj.CFBundleShortVersionString = newIosCFBundleShortVersionString;
-writeFileSync(infoFilePath, plist.build(obj))
+writeFileSync(infoFilePath, plist.build(obj));
