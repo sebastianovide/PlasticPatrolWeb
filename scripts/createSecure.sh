a0747f025797257c3a03ec7f65e00e6f15af8813
Joy Shan-Chun Kuo <shanchun.kuo@geovation.uk>
2021-04-01 11:05:42 +0100

    Fix App Store Connect issue: enable push notification capability (#338)

--- scripts/createSecure.sh
+++ scripts/createSecure.sh
@@ -9,7 +9,7 @@ pushd cordova-app
 
 # this is used for github
 # make sure that $ENC_PASSWORD is in github
-tar cvf secrets.tar build.json *.cer *.p12 *.mobileprovision *.jks GoogleService-Info.plist google-services.json GooglePlayAndroidDeveloper.json AppStoreConnectAPIKey.json
+tar cvf secrets.tar build.json *.cer *.p12 *.mobileprovision *.jks GooglePlayAndroidDeveloper.json AppStoreConnectAPIKey.json
 openssl enc -aes-256-cbc -in secrets.tar -out secrets.tar.encrypted -k $ENC_PASSWORD -md sha512
 
 # echo you should run this command
